c7baf3e24d29015bc381c5209eb29a45ad896107
Rafael Macedo <macedo.rafaelfernandes@gmail.com>
2014-12-24 11:56:24 -0200

    helper to alert messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def alert(level, message=nil, &block)
+    content_tag :div, class: "alert alert-#{level}", role: "alert" do
+      block_given? ? yield : message
+    end
+  end
 end
